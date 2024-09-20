dabe61a31021e0a91f388e573b6a07bc3e3b152a
kabornblack <shaibu.kabiru@email.com>
2024-09-20 14:35:32 +0300

    initial commit

---
+++ app/currentAffairs/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h2>Current things happening in the world</h2>
+    </div>
+  );
+}
+
+export default page;
